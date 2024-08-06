3d611744fb769c17e33ee9169fbd88c7bcecb5ac
BhanushreeNN <bhanushree1023@gmail.com>
2024-08-06 19:19:55 +0530

    update FormData2

--- src/components/FormData2.jsx
+++ src/components/FormData2.jsx
@@ -4,25 +4,40 @@ function FormData2() {
   const [fullname, setfullName] = useState({
     fname: "",
     lname: "",
+    email: "",
   });
-  var firstInputName, lastInputName;
 
   function InputChanged(event) {
     const { value, name } = event.target;
-    setfullName((prevValue) => {
-      //do not try to access event inside usestate hook
-      if (name === "fname") {
-        return {
-          fname: value,
-          lname: prevValue.lname,
-        };
-      } else if (name === "lname") {
-        return {
-          fname: prevValue.fname,
-          lname: value,
-        };
+    // setfullName((prevValue) => {
+    //   //do not try to access event inside usestate hook
+    //   if (name === "fname") {
+    //     return {
+    //       fname: value,
+    //       lname: prevValue.lname,
+    //       email: prevValue.email,
+    //     };
+    //   } else if (name === "lname") {
+    //     return {
+    //       fname: prevValue.fname,
+    //       lname: value,
+    //       email: prevValue.email,
+    //     };
+    //   } else if (name === "email") {
+    //     return {
+    //       fname: prevValue.fname,
+    //       lname: prevValue.lname,
+    //       email: value,
+    //     };
+    //   }
+    // });
+    setfullName((prevValue)=>{
+      //using SPREAD operator
+      return{
+        ...prevValue,
+        [name]: value,
       }
-    });
+    })
   }
 
   return (
@@ -30,6 +45,7 @@ function FormData2() {
       <h1>
         Hello {fullname.fname} {fullname.lname}!
       </h1>
+      <p>{fullname.email}</p>
       <form>
         <input
           onChange={InputChanged}
@@ -46,9 +62,42 @@ function FormData2() {
           name="lname"
           lvalue={fullname.lname}
         />
+        <br />
+        <input
+          onChange={InputChanged}
+          type="text"
+          placeholder=" E-mail"
+          name="email"
+          lvalue={fullname.email}
+        />
         <button className="button-56">Submit</button>
       </form>
     </div>
   );
 }
 export default FormData2;
+
+//**** SPREAD OPERATOR ****
+/*
+const veg = ["tomato", "potato"];
+const groceries = ["pulses", "fruits", ...veg]
+console.log(groceries);
+//output: ["pulses", "fruits", "tomato", "potato"]
+
+const fullname = {
+fname: "james",
+lname: "bond",
+};
+const user = {
+...fullname,
+id: 1,
+username: "james123",
+}
+console.log(user);
+//output: {
+fname: "james",
+lname: "bond",
+id: 1,
+username: "james123",
+}
+*/
